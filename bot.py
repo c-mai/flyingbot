f3a7b9e22237b1b7c3e2fac21199176f98d437ec
Conor Mai <conor.xu.mai@gmail.com>
2020-01-26 01:53:02 -0600

    asdf

---
+++ bot.py
@@ -0,0 +1,31 @@
+# bot.py
+import os
+import random
+
+from discord.ext import commands
+from dotenv import load_dotenv
+
+load_dotenv()
+token = os.getenv('DISCORD_TOKEN')
+
+bot = commands.Bot(command_prefix='!')
+
+@bot.command(name='99', help='Responds with a random quote from Brooklyn 99')
+async def nine_nine(ctx):
+    brooklyn_99_quotes = [
+        'I\'m the human form of the 💯 emoji.',
+        'Bingpot!',
+        (
+            'Cool. Cool cool cool cool cool cool cool, '
+            'no doubt no doubt no doubt no doubt.'
+        ),
+    ]
+
+    response = random.choice(brooklyn_99_quotes)
+    await ctx.send(response)
+
+@bot.command(name='groupvolunteer', help='Responds with a volunteering event')
+async def groupvolunteer(ctx):
+    await ctx.send("asdfjkal;sdf")
+    
+bot.run(token)
