fd402083e2a5f1632caef298c941ff58789d5da2
Conor Mai <conor.xu.mai@gmail.com>
2020-01-26 04:47:06 -0600

    just bot

--- bot.py
+++ bot.py
@@ -1,31 +1,33 @@
-# bot.py
+import discord
 import os
-import random
-
-from discord.ext import commands
 from dotenv import load_dotenv
+
 
 load_dotenv()
 token = os.getenv('DISCORD_TOKEN')
 
-bot = commands.Bot(command_prefix='!')
+def v_search(month, day, year):
+    return '01', '01', '1999'
 
-@bot.command(name='99', help='Responds with a random quote from Brooklyn 99')
-async def nine_nine(ctx):
-    brooklyn_99_quotes = [
-        'I\'m the human form of the 💯 emoji.',
-        'Bingpot!',
-        (
-            'Cool. Cool cool cool cool cool cool cool, '
-            'no doubt no doubt no doubt no doubt.'
-        ),
-    ]
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print('Logged on as {0}!'.format(self.user))
 
-    response = random.choice(brooklyn_99_quotes)
-    await ctx.send(response)
+    async def on_message(self, message):
+        if "!create" in message.content:
+            await message.channel.send("Great! I have these volunteer opportunities on that date. \nReact a :thumbsup: to add to calendar: ")
+            date = message.content.split()[1].split("-")
+            month, day, year = date
+            vlist = v_search(month, day, year)
 
-@bot.command(name='groupvolunteer', help='Responds with a volunteering event')
-async def groupvolunteer(ctx):
-    await ctx.send("asdfjkal;sdf")
+            for x in vlist:
+                await message.channel.send(x)
     
-bot.run(token)
+    async def on_raw_reaction_add(self, payload):
+        if str(payload.emoji) == "👍":
+           await payload.member.create_dm()
+           s = await client.get_channel(payload.channel_id).fetch_message(payload.message_id)
+           await payload.member.dm_channel.send("You have chosed to particpate:\n" + s.content)
+
+client = MyClient()
+client.run(token)
