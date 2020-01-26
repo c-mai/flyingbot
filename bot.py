191d0c4552162361f51248aa06aea5df7d4b1184
Conor Mai <conor.xu.mai@gmail.com>
2020-01-26 07:34:26 -0600

    hopefully final iteration

--- bot.py
+++ bot.py
@@ -1,33 +1,68 @@
 import discord
 import os
+import quickstart
+import csv
+
 from dotenv import load_dotenv
 
 
 load_dotenv()
 token = os.getenv('DISCORD_TOKEN')
 
-def v_search(month, day, year):
-    return '01', '01', '1999'
 
 class MyClient(discord.Client):
     async def on_ready(self):
         print('Logged on as {0}!'.format(self.user))
 
     async def on_message(self, message):
+        if message.author == client.user:
+            return
+        
         if "!create" in message.content:
-            await message.channel.send("Great! I have these volunteer opportunities on that date. \nReact a :thumbsup: to add to calendar: ")
-            date = message.content.split()[1].split("-")
-            month, day, year = date
-            vlist = v_search(month, day, year)
+            date = message.content.split()[1]
 
-            for x in vlist:
-                await message.channel.send(x)
+            v = False
+            with open('vlosheets.csv', 'r') as file:
+                file.readline()
+                for line in file:
+                    data = line.split(',')
+                    if(data[0] == date or data[0] == 'e'):
+                        event = "- " + date + " " + data[1] + " - " + data[2] + " : " + data[3] 
+                        await message.channel.send(event)
+                        v = True
+            
+            if not v:
+                await message.channel.send("There were no volunteer opportunities found for that date. Try creating a different event.")
+            else:
+                await message.channel.send("These opportunities were found for your date! \nReact a :thumbsup: to your favorite event to add to calendar.")
+
+                
+
     
     async def on_raw_reaction_add(self, payload):
         if str(payload.emoji) == "👍":
-           await payload.member.create_dm()
-           s = await client.get_channel(payload.channel_id).fetch_message(payload.message_id)
-           await payload.member.dm_channel.send("You have chosed to particpate:\n" + s.content)
+            dm = await payload.member.create_dm()
+            s = await client.get_channel(payload.channel_id).fetch_message(payload.message_id)
+            date = s.content.split()[1]
+            start = s.content.split()[2]
+            end = s.content.split()[4]
+            event = " ".join(s.content.split()[6:])
+
+            def check(m):
+                return m.channel == client.get_channel(dm.id) and m.author != client.user
+                
+            while True:
+                await payload.member.dm_channel.send("You have chosen to participate in:\n" + s.content + "\nPlease provide a working Gmail.")
+                email = await client.wait_for('message', check=check)
+                if "@gmail.com" in email.content:
+                    break
+
+            
+            quickstart.sendEvent(date, start, end, event, email.content)
+            await payload.member.dm_channel.send("Please check your Google calendar; an event should be created! \nThank you for giving back to the community!")
+
+           
+
 
 client = MyClient()
 client.run(token)
--- quickstart.py
+++ quickstart.py
@@ -12,7 +12,7 @@ from google.auth.transport.requests import Request
 SCOPES = ['https://www.googleapis.com/auth/calendar']
 
 
-def main():
+def sendEvent(date, stime, etime, title, email):
 
     """Shows basic usage of the Google Calendar API.
     Prints the start and name of the next 10 events on the user's calendar.
@@ -39,43 +39,20 @@ def main():
 
     service = build('calendar', 'v3', credentials=creds)
 
-    # # Call the Calendar API
-    # now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
-    # print('Getting the upcoming 10 events')
-    # events_result = service.events().list(calendarId='primary', timeMin=now,
-    #                                     maxResults=10, singleEvents=True,
-    #                                     orderBy='startTime').execute()
-    # events = events_result.get('items', [])
-    #
-    # if not events:
-    #     print('No upcoming events found.')
-    # for event in events:
-    #     start = event['start'].get('dateTime', event['start'].get('date'))
-    #     print(start, event['summary'])
-
-    # Refer to the Python quickstart on how to setup the environment:
-    # https://developers.google.com/calendar/quickstart/python
-    # Change the scope to 'https://www.googleapis.com/auth/calendar' and delete any
-    # stored credentials.
-
     event = {
-        'summary': 'Test Event WOOT',
-        'location': '800 Howard St., San Francisco, CA 94103',
+        'summary': title,
+        'location': 'Bryan, TX',
         'description': 'A chance to hear more about Google\'s developer products.',
         'start': {
-            'dateTime': '2020-01-26T02:00:00',
+            'dateTime': date.split("-")[-1] +"-"+date.split("-")[0]+"-"+date.split("-")[1] + 'T' + stime +':00',
             'timeZone': 'America/Chicago',
         },
         'end': {
-            'dateTime': '2020-01-26T04:00:00',
+            'dateTime': date.split("-")[-1] +"-"+date.split("-")[0]+"-"+date.split("-")[1] + 'T' + etime +':00',
             'timeZone': 'America/Chicago',
         },
-        'recurrence': [
-            'RRULE:FREQ=DAILY;COUNT=1'
-        ],
         'attendees': [
-            {'email': 'sabrinalu0916@gmail.com'},
-            #{'email': 'sbrin@example.com'},
+            {'email': email},
         ],
         'reminders': {
             'useDefault': False,
@@ -88,9 +65,3 @@ def main():
 
     event = service.events().insert(calendarId='primary', body=event).execute()
     print('Event created: %s' % (event.get('htmlLink')))
-
-
-
-
-if __name__ == '__main__':
-    main()
